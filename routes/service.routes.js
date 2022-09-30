d5dd64b77b3994662749703f5c34274e9c7f6464
Miriam Poletti <polettimiriam@gmail.com>
2022-09-30 17:44:29 +0200

    small changes in service post routes

--- routes/service.routes.js
+++ routes/service.routes.js
@@ -7,8 +7,8 @@ const Service = require('../models/Service.model');
 //  POST /api/services  - creates a new service 
 router.post('/services', (req, res, next) => {
   const { name, category, street, streetNr, complement, zip, website, email, phone, description, picture } = req.body;
- 
-  Service.create({ name, category, street, streetNr, complement, zip, website, email, phone, description, picture })
+ console.log(req.body)
+  Service.create( req.body )
     .then(response => res.json(response))
     .catch(err => res.json(err));
 });
