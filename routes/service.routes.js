c85d6839ccf460627649c0be1919e43f41bd0845
Miriam Poletti <polettimiriam@gmail.com>
2022-10-04 17:27:17 +0200

    added cloudinary config

--- routes/service.routes.js
+++ routes/service.routes.js
@@ -2,22 +2,41 @@ const router = require("express").Router();
 const mongoose = require('mongoose');
  
 const Service = require('../models/Service.model');
- 
+const fileUploader = require("../config/cloudinary.config");
+const cors = require('cors');
+
+
 
 //  POST /api/services  - creates a new service 
 router.post('/services', (req, res, next) => {
-  const { name, category, street, streetNr, complement, zip, website, email, phone, description, picture, isApproved, latitude, longitude, date, time } = req.body;
+  const { name, category, street, streetNr, complement, zip, website, email, phone, description, imageUrl, isApproved, latitude, longitude, date, time } = req.body;
  console.log(req.body)
   Service.create( req.body )
     .then(response => res.json(response))
     .catch(err => res.json(err));
 });
 
+
 //  GET /api/services -  Retrieves all of the services
 router.get("/services", (req, res, next) => {
   Service.find()
     .then((allServices) => res.json(allServices))
     .catch((err) => res.json(err));
+});
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
 });
 
 //  GET /api/services/:serviceId -  Retrieves a specific service by id
